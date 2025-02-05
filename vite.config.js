8661745759540f95894e1b4c15d7f376d7c7f766
kamy1992 <164549830+kamy1992@users.noreply.github.com>
2025-02-05 17:57:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card"
 
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+}
