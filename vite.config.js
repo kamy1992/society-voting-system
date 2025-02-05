20b7c22a309249974a3c95e4d4d23e518dfe0eda
kamy1992 <164549830+kamy1992@users.noreply.github.com>
2025-02-05 17:58:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card"
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
 
 export default defineConfig({
   plugins: [react()],
